d974bfab88eea6294d664db78b1d22851a9f59d6
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-03-21 13:20:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace class_Worker
+{
+    abstract class Worker
+    {
+        public abstract void Print();
+    }
+
+    class President : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am the president");
+        }
+    }
+
+    class Security : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am a security guard");
+        }
+    }
+
+    class Manager : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am a manager");
+        }
+    }
+
+    class Engineer : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am an engineer");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker president = new President();
+            president.Print();
+
+            Worker security = new Security();
+            security.Print();
+
+            Worker manager = new Manager();
+            manager.Print();
+
+            Worker engineer = new Engineer();
+            engineer.Print();
+        }
+    }
+}
